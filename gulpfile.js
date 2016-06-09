3cc5be57c1846ec879405772ae835f0d6baa3be3
Joey <joey.dube.1@ens.etsmtl.ca>
2016-06-09 16:35:21 -0400

    Empêcher Gulp de générer du CSS SASS

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,9 @@ var elixir = require('laravel-elixir');
  | for your Laravel application. By default, we are compiling the Sass
  | file for our application, as well as publishing vendor resources.
  |
- */
+
 
 elixir(function(mix) {
     mix.sass('app.scss');
 });
+ */
