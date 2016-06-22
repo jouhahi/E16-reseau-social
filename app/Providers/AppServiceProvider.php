6c22dd4fe88bdf592e96ab72e2efec5a808558db
Joey <joey.dube.1@ens.etsmtl.ca>
2016-06-22 16:30:24 -0400

    Tentative de faire fonctionner le HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //http://www.jeffmould.com/2016/01/31/laravel-5-2-forcing-https-routes-when-using-ssl/
+        if (!\App::environment('local')) {
+            \URL::forceSchema('https');
+        }
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -171,8 +171,6 @@ return [
 
         LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
         LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
-
-
     ],
 
     /*
