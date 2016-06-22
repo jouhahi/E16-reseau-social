fd874203453be80f8d2565007e3acb726533ebac
Joey <joey.dube.1@ens.etsmtl.ca>
2016-06-22 15:26:24 -0400

    BUGFIX les timestamp

--- database/seeds/OauthClientsTableSeeder.php
+++ database/seeds/OauthClientsTableSeeder.php
@@ -12,18 +12,20 @@
 use App\User;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
+use Carbon\Carbon;
 
 class OauthClientsTableSeeder extends Seeder
 {
     public function run()
     {
+        $carbon = Carbon::now();
         //Ajout des informations pour le client oauth de l'application mobile
         DB::table('oauth_clients')->insert([
             'id' => 'f3d259ddd3ed8ff3843839b',
             'secret' => '4c7f6f8fa93d59c45502c0ae8c4a95b',
             'name'=> 'Application mobile',
-            'created_at'=>'2015–05–12 21:00:00',
-            'updated_at'=>'2015–05–12 21:00:00'
+            'created_at'=>$carbon,
+            'updated_at'=>$carbon
         ]);
 
         //Ajout des informations pour le client oauth du site de vente de billets
@@ -31,15 +33,15 @@ class OauthClientsTableSeeder extends Seeder
             'id' => '32k4h34jk2h34kj2h34kj2jk',
             'secret' => '45kjh36kvjhnk54vvhj3kj64j6h3jk4g2k',
             'name'=> 'Ticket-fire',
-            'created_at'=>'2015–05–12 21:00:00',
-            'updated_at'=>'2015–05–12 21:00:00'
+            'created_at'=>$carbon,
+            'updated_at'=>$carbon
         ]);
 
         DB::table('oauth_client_endpoints')->insert([
         'client_id' => '32k4h34jk2h34kj2h34kj2jk',
         'redirect_uri' => 'https://ticket-fire.herokuapp.com/federation',
-        'created_at'=>'2015–05–12 21:00:00',
-        'updated_at'=>'2015–05–12 21:00:00'
+        'created_at'=>$carbon,
+        'updated_at'=>$carbon
     ]);
 
     }
