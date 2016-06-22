5d3a9c78d916d87a6242184cffabac31257fd5ab
Joey <joey.dube.1@ens.etsmtl.ca>
2016-06-22 16:53:46 -0400

    BUGFIX hhts

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,7 +16,7 @@ class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && env('APP_ENV') === 'production') {
+        if (!$request->secure() && !\App::environment('local')) {
             return redirect()->secure($request->getRequestUri());
         }
 
