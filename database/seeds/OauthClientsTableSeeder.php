6e50288c0311413d1dd84af33c10c2ab21bf7580
Joey <joey.dube.1@ens.etsmtl.ca>
2016-06-22 15:21:50 -0400

    BUGFIX PostgaySQL marche pô

--- database/seeds/OauthClientsTableSeeder.php
+++ database/seeds/OauthClientsTableSeeder.php
@@ -23,7 +23,7 @@ class OauthClientsTableSeeder extends Seeder
             'secret' => '4c7f6f8fa93d59c45502c0ae8c4a95b',
             'name'=> 'Application mobile',
             'created_at'=>'2015–05–12 21:00:00',
-            'updated_at'=>'0000–00–00 00:00:00'
+            'updated_at'=>'2015–05–12 21:00:00'
         ]);
 
         //Ajout des informations pour le client oauth du site de vente de billets
@@ -32,14 +32,14 @@ class OauthClientsTableSeeder extends Seeder
             'secret' => '45kjh36kvjhnk54vvhj3kj64j6h3jk4g2k',
             'name'=> 'Ticket-fire',
             'created_at'=>'2015–05–12 21:00:00',
-            'updated_at'=>'0000–00–00 00:00:00'
+            'updated_at'=>'2015–05–12 21:00:00'
         ]);
 
         DB::table('oauth_client_endpoints')->insert([
         'client_id' => '32k4h34jk2h34kj2h34kj2jk',
         'redirect_uri' => 'https://ticket-fire.herokuapp.com/federation',
         'created_at'=>'2015–05–12 21:00:00',
-        'updated_at'=>'0000–00–00 00:00:00'
+        'updated_at'=>'2015–05–12 21:00:00'
     ]);
 
     }
